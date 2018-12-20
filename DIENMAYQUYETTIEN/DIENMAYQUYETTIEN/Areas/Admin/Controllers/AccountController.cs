64618a8723ccc63275ea44004201edb055f68fa7
thuantran73 <larten1234@gmail.com>
2018-12-20 13:49:26 +0700

    1st commit

---
+++ DIENMAYQUYETTIEN/DIENMAYQUYETTIEN/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DIENMAYQUYETTIEN.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Admin/Account
+        public ActionResult Login()  
+        {  
+            return View();  
+        }  
+  
+        [HttpPost]  
+        [ValidateAntiForgeryToken]  
+        public ActionResult Login(Models.Account objUser)   
+        {  
+            if (ModelState.IsValid)   
+            {  
+                using(Models.DIENMAYQUYETTIENEntities db = new Models.DIENMAYQUYETTIENEntities())  
+                {  
+                    var obj = db.Accounts.Where(a => a.Username.Equals(objUser.Username) && a.Password.Equals(objUser.Password)).FirstOrDefault();  
+                    if (obj != null)  
+                    {  
+                        
+                        Session["UserName"] = obj.FullName.ToString();  
+                        return RedirectToAction("Index", "ProductAdmin");  
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid username or password.");
+                    }
+                }  
+            }  
+            return View(objUser);  
+        }  
+  
+        public ActionResult UserDashBoard()  
+        {
+            return View(); 
+        }
+        
+
+    }
+
+    
+}
---
+++ DIENMAYQUYETTIEN/DIENMAYQUYETTIEN/Areas/Admin/Controllers/ProductAdminController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DIENMAYQUYETTIEN.Models;
+using System.Net;
+
+namespace DIENMAYQUYETTIEN.Areas.Admin.Controllers
+{
+    public class ProductAdminController : Controller
+    {
+        DIENMAYQUYETTIENEntities db = new DIENMAYQUYETTIENEntities();
+        //
+        // GET: /Admin/ProductAdmin/
+        public ActionResult Index(){
+            var product = db.Products.OrderByDescending(x => x.ID).ToList();
+            return View(product);
+        }
+        
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Prod = db.ProductTypes.OrderByDescending(x => x.ID).ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            var pro = new Product();
+            pro.ProductCode = p.ProductCode;
+            pro.ProductName = p.ProductName;
+            pro.SalePrice = p.SalePrice;
+            pro.OriginPrice = p.OriginPrice;
+            pro.InstallmentPrice = p.InstallmentPrice;
+            pro.Quantity = p.Quantity;
+            pro.Status = p.Status;
+            pro.ProductTypeID = p.ProductTypeID;
+            db.Products.Add(pro);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        
+        
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Prod = db.ProductTypes.OrderByDescending(x => x.ID).ToList();
+            Product model = db.Products.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            var pro = new Product();
+            pro.ProductCode = p.ProductCode;
+            pro.ProductName = p.ProductName;
+            pro.SalePrice = p.SalePrice;
+            pro.OriginPrice = p.OriginPrice;
+            pro.InstallmentPrice = p.InstallmentPrice;
+            pro.Quantity = p.Quantity;
+            pro.Status = p.Status;
+            pro.ProductTypeID = p.ProductTypeID;
+            db.Products.Add(pro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            ViewBag.Prod = db.ProductTypes.OrderByDescending(x => x.ID).ToList();
+            
+            Product model = db.Products.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // POST: /BangSanPham/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product model = db.Products.Find(id);
+            db.Products.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
