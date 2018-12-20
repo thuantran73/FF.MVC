dad2ec0c28c0f43db96e0296de3a1ec4f2fa20df
thuantran73 <larten1234@gmail.com>
2018-12-20 15:02:53 +0700

    Installment Cash Bill UI

---
+++ DIENMAYQUYETTIEN/DIENMAYQUYETTIEN/Areas/Admin/Controllers/InstallmentCashBillController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DIENMAYQUYETTIEN.Models;
+
+namespace DIENMAYQUYETTIEN.Areas.Admin.Controllers
+{
+    public class InstallmentCashBillController : Controller
+    {
+        private DIENMAYQUYETTIENEntities db = new DIENMAYQUYETTIENEntities();
+
+        // GET: Admin/InstallmentCashBill
+        public ActionResult Index()
+        {
+            var installmentBills = db.InstallmentBills.Include(i => i.Customer);
+            return View(installmentBills.ToList());
+        }
+
+        // GET: Admin/InstallmentCashBill/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBill installmentBill = db.InstallmentBills.Find(id);
+            if (installmentBill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installmentBill);
+        }
+
+        // GET: Admin/InstallmentCashBill/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "CustomerCode");
+            return View();
+        }
+
+        // POST: Admin/InstallmentCashBill/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,BillCode,CustomerID,Date,Shipper,Note,Method,Period,GrandTotal,Taken,Remain")] InstallmentBill installmentBill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.InstallmentBills.Add(installmentBill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "CustomerCode", installmentBill.CustomerID);
+            return View(installmentBill);
+        }
+
+        // GET: Admin/InstallmentCashBill/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBill installmentBill = db.InstallmentBills.Find(id);
+            if (installmentBill == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "CustomerCode", installmentBill.CustomerID);
+            return View(installmentBill);
+        }
+
+        // POST: Admin/InstallmentCashBill/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,BillCode,CustomerID,Date,Shipper,Note,Method,Period,GrandTotal,Taken,Remain")] InstallmentBill installmentBill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(installmentBill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "CustomerCode", installmentBill.CustomerID);
+            return View(installmentBill);
+        }
+
+        // GET: Admin/InstallmentCashBill/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBill installmentBill = db.InstallmentBills.Find(id);
+            if (installmentBill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installmentBill);
+        }
+
+        // POST: Admin/InstallmentCashBill/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            InstallmentBill installmentBill = db.InstallmentBills.Find(id);
+            db.InstallmentBills.Remove(installmentBill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
