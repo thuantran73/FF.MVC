6e5f3e6fcdf8333b02c0383d0ccb321710709d49
thuantran73 <larten1234@gmail.com>
2018-12-20 16:37:30 +0700

    Installment & Cash Bill details

---
+++ DIENMAYQUYETTIEN/DIENMAYQUYETTIEN/Areas/Admin/Controllers/CashBillsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DIENMAYQUYETTIEN.Models;
+
+namespace DIENMAYQUYETTIEN.Areas.Admin.Controllers
+{
+    public class CashBillsController : Controller
+    {
+        private DIENMAYQUYETTIENEntities db = new DIENMAYQUYETTIENEntities();
+
+        // GET: Admin/CashBills
+        public ActionResult Index()
+        {
+            return View(db.CashBills.ToList());
+        }
+
+        // GET: Admin/CashBills/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CashBill cashBill = db.CashBills.Find(id);
+            if (cashBill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cashBill);
+        }
+
+        // GET: Admin/CashBills/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/CashBills/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,BillCode,CustomerName,PhoneNumber,Address,Date,Shipper,Note,GrandTotal")] CashBill cashBill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CashBills.Add(cashBill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cashBill);
+        }
+
+        // GET: Admin/CashBills/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CashBill cashBill = db.CashBills.Find(id);
+            if (cashBill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cashBill);
+        }
+
+        // POST: Admin/CashBills/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,BillCode,CustomerName,PhoneNumber,Address,Date,Shipper,Note,GrandTotal")] CashBill cashBill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cashBill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cashBill);
+        }
+
+        // GET: Admin/CashBills/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CashBill cashBill = db.CashBills.Find(id);
+            if (cashBill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cashBill);
+        }
+
+        // POST: Admin/CashBills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CashBill cashBill = db.CashBills.Find(id);
+            db.CashBills.Remove(cashBill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
