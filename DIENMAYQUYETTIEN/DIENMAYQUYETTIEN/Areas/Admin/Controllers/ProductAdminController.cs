dfb480973cefdc9e1203a283c1b86ae68456dca6
thuantran73 <larten1234@gmail.com>
2018-12-21 11:37:27 +0700

    Upload picture

--- DIENMAYQUYETTIEN/DIENMAYQUYETTIEN/Areas/Admin/Controllers/ProductAdminController.cs
+++ DIENMAYQUYETTIEN/DIENMAYQUYETTIEN/Areas/Admin/Controllers/ProductAdminController.cs
@@ -1,100 +1,160 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using DIENMAYQUYETTIEN.Models;
-using System.Net;
+using System.Transactions;
 
-namespace DIENMAYQUYETTIEN.Areas.Admin.Controllers
+namespace WebApplication.Controllers
 {
-    public class ProductAdminController : Controller
+    public class DanhSachSanPhamController : Controller
     {
         DIENMAYQUYETTIENEntities db = new DIENMAYQUYETTIENEntities();
-        //
-        // GET: /Admin/ProductAdmin/
-        public ActionResult Index(){
-            var product = db.Products.OrderByDescending(x => x.ID).ToList();
-            return View(product);
+
+        // GET: /DanhSachSanPham/
+        public ActionResult Index()
+        {
+            var product = db.Products.Include(b => b.ProductType);
+            return View(product.ToList());
         }
-        
-        [HttpGet]
+
+        // GET: /DanhSachSanPham/Details/5
+        public FileResult Details(string id)
+        {
+            var path = Server.MapPath("~/App_Data");
+            path = System.IO.Path.Combine(path, id);
+            return File(path, "images");
+        }
+
+        // GET: /DanhSachSanPham/Create
         public ActionResult Create()
         {
-            ViewBag.Prod = db.ProductTypes.OrderByDescending(x => x.ID).ToList();
+            ViewBag.Prod_ID = new SelectList(db.ProductTypes, "ID", "ProductTypeName");
             return View();
         }
-        [HttpPost]
-        public ActionResult Create(Product p)
-        {
-            var pro = new Product();
-            pro.ProductCode = p.ProductCode;
-            pro.ProductName = p.ProductName;
-            pro.SalePrice = p.SalePrice;
-            pro.OriginPrice = p.OriginPrice;
-            pro.InstallmentPrice = p.InstallmentPrice;
-            pro.Quantity = p.Quantity;
-            pro.Status = p.Status;
-            pro.ProductTypeID = p.ProductTypeID;
-            db.Products.Add(pro);
-            db.SaveChanges();
 
-            return RedirectToAction("Index");
+        // POST: /DanhSachSanPham/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product model)
+        {
+            CheckProducts(model);
+            if (ModelState.IsValid)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    db.Products.Add(model);
+                    db.SaveChanges();
+
+                    if (Request.Files["Picture"] != null &&
+                        Request.Files["Picture"].ContentLength > 0)
+                    {
+                        var path = Server.MapPath("~/App_Data");
+                        path = System.IO.Path.Combine(path, model.ID.ToString());
+                        Request.Files["Picture"].SaveAs(path);
+
+                        scope.Complete();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                        ModelState.AddModelError("HinhAnh", "Chua chon hinh anh cho san pham");
+                }
+            }
+
+            ViewBag.Prod_ID = new SelectList(db.ProductTypes, "id", "TenLoai", model.ProductTypeID);
+            return View(model);
         }
 
-        
-        
+        private void CheckProducts(Product model)
+        {
+            if (model.OriginPrice < 0)
+                ModelState.AddModelError("GiaGoc", "Gia goc phai lon hon 0");
+            if (model.OriginPrice > model.SalePrice)
+                ModelState.AddModelError("GiaBan", "Gia ban phai lon hon gia goc");
+            if (model.OriginPrice > model.InstallmentPrice)
+                ModelState.AddModelError("GiaGop", "Gia gop phai lon hon gia goc");
+            if (model.Quantity < 0)
+                ModelState.AddModelError("SoLuongTon", "So luong ton phai lon hon 0");
+        }
+
+        // GET: /DanhSachSanPham/Edit/5
         public ActionResult Edit(int id)
         {
-            ViewBag.Prod = db.ProductTypes.OrderByDescending(x => x.ID).ToList();
             Product model = db.Products.Find(id);
             if (model == null)
             {
                 return HttpNotFound();
             }
-            
+            ViewBag.Loai_id = new SelectList(db.ProductTypes, "id", "TenLoai", model.ProductTypeID);
             return View(model);
         }
 
+        // POST: /DanhSachSanPham/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public ActionResult Edit(Product p)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product model)
         {
-            var pro = new Product();
-            pro.ProductCode = p.ProductCode;
-            pro.ProductName = p.ProductName;
-            pro.SalePrice = p.SalePrice;
-            pro.OriginPrice = p.OriginPrice;
-            pro.InstallmentPrice = p.InstallmentPrice;
-            pro.Quantity = p.Quantity;
-            pro.Status = p.Status;
-            pro.ProductTypeID = p.ProductTypeID;
-            db.Products.Add(pro);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        public ActionResult Delete(int id)
-        {
-            ViewBag.Prod = db.ProductTypes.OrderByDescending(x => x.ID).ToList();
-            
-            Product model = db.Products.Find(id);
-            if (model == null)
+            CheckProducts(model);
+            if (ModelState.IsValid)
             {
-                return HttpNotFound();
+                using (var scope = new TransactionScope())
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    var path = Server.MapPath("~/App_Data");
+                    path = path + "/" + model.ID;
+                    if (Request.Files["Picture"] != null &&
+                        Request.Files["Picture"].ContentLength > 0)
+                    {
+                        Request.Files["Picture"].SaveAs(path);
+                    }
+
+                    scope.Complete(); // approve for transaction
+                    return RedirectToAction("Index");
+                }
             }
+            ViewBag.Loai_id = new SelectList(db.ProductTypes, "id", "TenLoai", model.ProductTypeID);
             return View(model);
         }
 
-        // POST: /BangSanPham/Delete/5
+        // GET: /DanhSachSanPham/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product bangsanpham = db.Products.Find(id);
+            if (bangsanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bangsanpham);
+        }
+
+        // POST: /DanhSachSanPham/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Product model = db.Products.Find(id);
-            db.Products.Remove(model);
+            Product bangsanpham = db.Products.Find(id);
+            db.Products.Remove(bangsanpham);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
