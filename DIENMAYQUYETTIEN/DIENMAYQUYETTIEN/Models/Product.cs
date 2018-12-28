39cf275660a44ad1d1e618471dc986b5575bffd5
thuantran73 <larten1234@gmail.com>
2018-12-28 17:37:05 +0700

    Complete

--- DIENMAYQUYETTIEN/DIENMAYQUYETTIEN/Models/Product.cs
+++ DIENMAYQUYETTIEN/DIENMAYQUYETTIEN/Models/Product.cs
@@ -11,7 +11,8 @@ namespace DIENMAYQUYETTIEN.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Product
     {
         public Product()
